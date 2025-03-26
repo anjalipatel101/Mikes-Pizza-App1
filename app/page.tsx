8299287d5432410469d4c248f0919a8a0932dca1
Haothai1 <Hao2003thai@gmail.com>
2025-03-26 13:43:58 -0700

    handle null content for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 "use client";
-
+import React from "react";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import "./globals.css";
+import "./../app/app.css";
 // import { generateClient } from "aws-amplify/data";
 // import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
 // import { Amplify } from "aws-amplify";
 // import outputs from "@/amplify_outputs.json";
 // import "@aws-amplify/ui-react/styles.css";
-import React from "react";
 
 // Amplify.configure(outputs);
 // const client = generateClient<Schema>();
