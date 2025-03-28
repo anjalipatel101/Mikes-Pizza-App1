c03425af1eabe330c1beb953d66e479ba718afb5
Haothai1 <Hao2003thai@gmail.com>
2025-03-27 17:31:51 -0700

    added sign in and sign up page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,8 +13,8 @@ const Navbar = () => {
 			<ul className="nav-ul">
 				<Link href="/menu" className="nav-links">Menu</Link>
 				<Link href="/track-order" className="nav-links">Track Order</Link>
-				<Link href="/login" className="nav-links">Sign In</Link>
-				<Link href="/signup" className="nav-links">Sign Up</Link>
+				<Link href="https://us-east-2yzahmurmt.auth.us-east-2.amazoncognito.com/login?client_id=9mka5k1capi3rlsv8sfurq13k&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fmain.d3w16m2ou703w5.amplifyapp.com%2F" className="nav-links">Sign In</Link>
+				<Link href="https://us-east-2yzahmurmt.auth.us-east-2.amazoncognito.com/signup?client_id=9mka5k1capi3rlsv8sfurq13k&redirect_uri=https%3A%2F%2Fmain.d3w16m2ou703w5.amplifyapp.com%2F&response_type=code&scope=email+openid+phone" className="nav-links">Sign Up</Link>
 				<Link href="/cart" className="nav-links"><div className="nav-cart-btn"><FaShoppingCart className="nav-cart-icon" /></div></Link>
 			</ul>
 		</nav>
